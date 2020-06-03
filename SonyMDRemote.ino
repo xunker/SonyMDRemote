d8104cfc3582871d2287e70faf84f40644cd5bfe
Matthew Nielsen <xunker@pyxidis.org>
2020-06-03 14:43:19 -0600

    byte decoding test

--- SonyMDRemote.ino
+++ SonyMDRemote.ino
@@ -1,4 +1,4 @@
-#define REMOTE_DATA_PIN 3
+#define REMOTE_DATA_PIN A0
 
 volatile boolean pinLow = false;
 void dataPinLow() {
@@ -8,6 +8,7 @@ void dataPinLow() {
 void setup() {
   // pinMode(REMOTE_DATA_PIN, INPUT_PULLUP);
   pinMode(REMOTE_DATA_PIN, INPUT);
+  // pinMode(REMOTE_DATA_PIN, INPUT_PULLUP);
 
   // attachInterrupt(digitalPinToInterrupt(REMOTE_DATA_PIN), dataPinChanged, CHANGE);
 
@@ -30,46 +31,228 @@ unsigned long wentHighAt = 0;
 
 unsigned long lastFrameAtMicros = 0;
 
+unsigned int toNearest(uint8_t nearest, unsigned int num) {
+  return ((num + (nearest / 2)) / nearest) * nearest;
+}
+
+/*
+NEXT IDEA:
+Just monitor for the low time of every pulse.
+* If it's a reset pulse (>800us), clear the current command buffer.
+* If it's less than a reset pulse, work out of it's a 0 or a one and append it to the current command buffer.
+
+Keep track of the last time we've been pulled low and if it's greater than the reset pulse length then execute what's in the command buffer.
+
+TODO: reject pulses beyond maximum
+
+*/
+
+/* --- polling for high and low state times --- */
+
+
+String frameBuffer = "";
+unsigned long currentLowStartedAt = 0;
+unsigned long currentLowEndedAt = 0;
+unsigned long currentHighStartedAt = 0;
+unsigned long currentHighEndedAt = 0;
+
 void loop() {
-  if (wentLowAt > 0) {
-    if (digitalRead(REMOTE_DATA_PIN) == HIGH) {
-      wentHighAt = micros();
-      digitalWrite(LED_BUILTIN, LOW);
-
-      unsigned int lowForMicros = wentHighAt - wentLowAt;
-
-      if (lowForMicros > FRAME_BEGIN_PULSE_MICROS) {
-        // digitalWrite(LED_BUILTIN, HIGH);
-        // Serial.println(lowForMicros);
-
-        unsigned int startedWaiting = micros();
-
-        while (digitalRead(REMOTE_DATA_PIN) == HIGH) { }
-        unsigned int highForMicros = micros() - startedWaiting;
-
-        if (highForMicros > FRAME_BEGIN_PULSE_MICROS) {
-          Serial.println("");
-          Serial.print(lowForMicros);
-          Serial.print("-");
-          Serial.print(highForMicros);
-          Serial.print("(");
-          Serial.print((micros() - lastFrameAtMicros)/1000);
-          Serial.print(")");
-          lastFrameAtMicros = wentLowAt;
-        }
-      } else {
-        Serial.print(".");
-      }
-      wentLowAt = 0;
-    }
-  } else {
-    if (digitalRead(REMOTE_DATA_PIN) == LOW) {
-      wentLowAt = micros();
-      wentHighAt = 0;
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
+  while (digitalRead(REMOTE_DATA_PIN) == LOW) {
+    if (currentLowStartedAt == 0)
+      currentLowStartedAt = micros();
+    if (currentHighStartedAt > 0)
+      currentHighStartedAt = 0;
   }
 
+  currentLowEndedAt = micros();
 
-  // digitalWrite(LED_BUILTIN, LOW);
+  unsigned long currentLowLength = currentLowEndedAt - currentLowStartedAt;
+  boolean framestart = false;
+  if ((currentLowLength > FRAME_BEGIN_PULSE_MICROS) && (currentLowLength <= FRAME_BEGIN_PULSE_MICROS * 2)){
+    Serial.println(frameBuffer);
+    frameBuffer = "";
+    framestart = true;
+  }
+
+  // frameBuffer.concat("_");
+  // frameBuffer.concat(currentLowLength);
+  // frameBuffer.concat(" ");
+
+  while (digitalRead(REMOTE_DATA_PIN) == HIGH) {
+    if (currentHighStartedAt == 0)
+      currentHighStartedAt = micros();
+    if (currentLowStartedAt > 0)
+      currentLowStartedAt = 0;
+  }
+
+  currentHighEndedAt = micros();
+
+  unsigned long currentHighLength = currentHighEndedAt - currentHighStartedAt;
+  // frameBuffer.concat("-");
+  // frameBuffer.concat(currentHighLength);
+  // frameBuffer.concat(" ");
+
+  if ((!framestart) && (currentLowLength <= FRAME_BEGIN_PULSE_MICROS * 2)) {
+    if (currentLowLength <= 20) {
+      frameBuffer.concat(1);
+    } else if (currentLowLength <= 230) {
+      frameBuffer.concat(0);
+    } else {
+      frameBuffer.concat("_");
+      frameBuffer.concat(currentLowLength);
+      frameBuffer.concat("-");
+      frameBuffer.concat(currentHighLength);
+      frameBuffer.concat(" ");
+    }
+  }
 }
+
+  /* --- pulsein --- */
+
+  // String frameBuffer = "";
+  // unsigned long currentPulseEndedAtMicros = 0;
+  // unsigned long previousPulseEndedAtMicros = 0;
+
+  // void loop() {
+  //   unsigned int pulseLengthMicros = pulseIn(REMOTE_DATA_PIN, LOW);
+  //   if (pulseLengthMicros > 0) {
+  //     currentPulseEndedAtMicros = micros();
+
+  //     if (pulseLengthMicros > FRAME_BEGIN_PULSE_MICROS) {
+  //       Serial.println(frameBuffer);
+  //       frameBuffer = "";
+
+  //       // frameBuffer = "";
+  //       // frameBuffer.concat(pulseLengthMicros);
+  //       // frameBuffer.concat(" ");
+  //     } else {
+
+  //       // // frameBuffer.concat(pulseLengthMicros);
+  //       // // frameBuffer.concat("-");
+  //       // if (pulseLengthMicros > 170) {
+  //       //   frameBuffer.concat(0);
+  //       // } else {
+  //       //   frameBuffer.concat(1);
+  //       // }
+  //     }
+
+  //     frameBuffer.concat(toNearest(10, pulseLengthMicros));
+  //     if (pulseLengthMicros < FRAME_BEGIN_PULSE_MICROS) {
+  //       frameBuffer.concat("/");
+  //       frameBuffer.concat(toNearest(10, currentPulseEndedAtMicros - previousPulseEndedAtMicros));
+  //       frameBuffer.concat("-");
+  //       frameBuffer.concat(toNearest(10, (currentPulseEndedAtMicros - previousPulseEndedAtMicros) - pulseLengthMicros));
+  //     }
+  //     frameBuffer.concat("\t");
+  //   }
+
+  //   previousPulseEndedAtMicros = currentPulseEndedAtMicros;
+  // }
+
+  /* --- polling --- */
+
+  // String frameBuffer = "";
+
+  // void loop()
+  // {
+  //   if (digitalRead(REMOTE_DATA_PIN) == LOW) {
+  //     wentLowAt = micros();
+  //     while (digitalRead(REMOTE_DATA_PIN) == LOW) { }
+
+  //     unsigned int pulseLengthMicros = micros() - wentLowAt;
+  //     if (pulseLengthMicros > FRAME_BEGIN_PULSE_MICROS) {
+  //       Serial.println(frameBuffer);
+  //       frameBuffer = "";
+
+  //       // frameBuffer = "";
+  //       // frameBuffer.concat(pulseLengthMicros);
+  //       // frameBuffer.concat(" ");
+  //     } else {
+
+  //       // // frameBuffer.concat(pulseLengthMicros);
+  //       // // frameBuffer.concat("-");
+  //       // if (pulseLengthMicros > 170) {
+  //       //   frameBuffer.concat(0);
+  //       // } else {
+  //       //   frameBuffer.concat(1);
+  //       // }
+  //     }
+
+  //     frameBuffer.concat(pulseLengthMicros);
+  //     frameBuffer.concat(" ");
+  //   }
+  // }
+
+  /* --- */
+
+  // #define FB_LENGTH 128
+  // unsigned short frameBuffer[FB_LENGTH];
+  // uint8_t fbIndex = 0;
+
+  // void loop() {
+  //   if (digitalRead(REMOTE_DATA_PIN) == LOW) {
+  //     wentLowAt = micros();
+  //     while (digitalRead(REMOTE_DATA_PIN) == LOW) { }
+
+  //     unsigned int pulseLengthMicros = micros() - wentLowAt;
+  //     if (pulseLengthMicros > FRAME_BEGIN_PULSE_MICROS) {
+  //       fbIndex = 0;
+
+  //       Serial.println("");
+  //       for (uint8_t i = 0; i < FB_LENGTH; i++) {
+  //         if (frameBuffer[i] > 0) {
+  //           Serial.print(frameBuffer[i]);
+  //           Serial.print(" ");
+  //         }
+  //       }
+  //     }
+
+  //     frameBuffer[fbIndex] = pulseLengthMicros;
+  //     fbIndex++;
+  //     if (fbIndex >= FB_LENGTH)
+  //       Serial.println("FRAMEBUFFER INDEX OUT OF BOUNDS!");
+  //   }
+  // }
+
+  // void loop() {
+  //   if (wentLowAt > 0) {
+  //     if (digitalRead(REMOTE_DATA_PIN) == HIGH) {
+  //       wentHighAt = micros();
+  //       digitalWrite(LED_BUILTIN, LOW);
+
+  //       unsigned int lowForMicros = wentHighAt - wentLowAt;
+
+  //       if (lowForMicros > FRAME_BEGIN_PULSE_MICROS) {
+  //         // digitalWrite(LED_BUILTIN, HIGH);
+  //         // Serial.println(lowForMicros);
+
+  //         unsigned int startedWaiting = micros();
+
+  //         while (digitalRead(REMOTE_DATA_PIN) == HIGH) { }
+  //         unsigned int highForMicros = micros() - startedWaiting;
+
+  //         if (highForMicros > FRAME_BEGIN_PULSE_MICROS) {
+  //           Serial.println("");
+  //           Serial.print(lowForMicros);
+  //           Serial.print("-");
+  //           Serial.print(highForMicros);
+  //           Serial.print("(");
+  //           Serial.print((micros() - lastFrameAtMicros)/1000);
+  //           Serial.print(")");
+  //           lastFrameAtMicros = wentLowAt;
+  //         }
+  //       } else {
+  //         Serial.print(".");
+  //       }
+  //       wentLowAt = 0;
+  //     }
+  //   } else {
+  //     if (digitalRead(REMOTE_DATA_PIN) == LOW) {
+  //       wentLowAt = micros();
+  //       wentHighAt = 0;
+  //       digitalWrite(LED_BUILTIN, HIGH);
+  //     }
+  //   }
+
+  //   // digitalWrite(LED_BUILTIN, LOW);
+  // }
